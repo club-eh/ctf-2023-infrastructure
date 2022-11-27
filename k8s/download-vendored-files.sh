760d3c240030138e31e2a48ca3a1926f746ea0a8
sudoBash418 <sudoBash418@gmail.com>
2022-11-26 21:49:46 -0700

    Remove OLM in favour of vendored YAML manifests
    Also:
    - Update ECK version
    - Add Prometheus operator

--- k8s/download-vendored-files.sh
+++ k8s/download-vendored-files.sh
@@ -4,10 +4,11 @@
 
 set -e
 
-VERSION_OLM="0.22.0"
+#VERSION_OLM="0.22.0"
 VERSION_INGRESS_NGINX="1.5.1"
 VERSION_KUBE_DASHBOARD="2.7.0"
-VERSION_ECK="2.4.0"
+VERSION_ECK="2.5.0"
+VERSION_PROM_OPERATOR="0.61.1"
 
 
 dl() { wget -nv --show-progress "$@"; }
@@ -16,12 +17,14 @@ cd "$(dirname "$(realpath "$0")")"
 
 mkdir -p resources/vendor
 
-dl -O resources/vendor/olm-crds.yaml "https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v${VERSION_OLM}/crds.yaml"
-dl -O resources/vendor/olm.yaml "https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v${VERSION_OLM}/olm.yaml"
+#dl -O resources/vendor/olm-crds.yaml "https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v${VERSION_OLM}/crds.yaml"
+#dl -O resources/vendor/olm.yaml "https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v${VERSION_OLM}/olm.yaml"
 
 dl -O resources/vendor/ingress-nginx.yaml "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v${VERSION_INGRESS_NGINX}/deploy/static/provider/cloud/deploy.yaml"
 
 dl -O resources/vendor/kube-dashboard.yaml "https://raw.githubusercontent.com/kubernetes/dashboard/v${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml"
 
-#dl -O resources/vendor/eck-crds.yaml "https://download.elastic.co/downloads/eck/${VERSION_ECK}/crds.yaml"
-#dl -O resources/vendor/eck-operator.yaml "https://download.elastic.co/downloads/eck/${VERSION_ECK}/operator.yaml"
+dl -O resources/vendor/eck-crds.yaml "https://download.elastic.co/downloads/eck/${VERSION_ECK}/crds.yaml"
+dl -O resources/vendor/eck-operator.yaml "https://download.elastic.co/downloads/eck/${VERSION_ECK}/operator.yaml"
+
+dl -O resources/vendor/prometheus-operator.yaml "https://github.com/prometheus-operator/prometheus-operator/releases/download/v${VERSION_PROM_OPERATOR}/bundle.yaml"
--- k8s/resources/operators/readme.txt
+++
@@ -1,1 +0,0 @@
-This directory does not represent a namespace; it contains all OLM operator subscriptions.
