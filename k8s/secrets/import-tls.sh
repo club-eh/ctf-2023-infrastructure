eac119365e9863932355d0862eb82ebf7fad3411
sudoBash418 <sudoBash418@gmail.com>
2022-11-28 00:58:36 -0700

    k8s/secrets: Add import script for TLS certs
    Note: repo doesn't currently include the actual certs/keys.

---
+++ k8s/secrets/import-tls.sh
@@ -0,0 +1,4 @@
+# ctf-2023.clubeh.ca
+kubectl create secret tls le-ctf-2023-keypair --cert=../secrets/.lego/certificates/ctf-2023.clubeh.ca.crt --key=../secrets/.lego/certificates/ctf-2023.clubeh.ca.key
+# admin.ctf-2023.clubeh.ca
+kubectl create secret tls -n monitoring le-admin-ctf-2023-keypair --cert=../secrets/.lego/certificates/admin.ctf-2023.clubeh.ca.crt --key=../secrets/.lego/certificates/admin.ctf-2023.clubeh.ca.key
