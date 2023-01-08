abfea8becc667e225f6896b037fc8d468199e12b
sudoBash418 <sudoBash418@gmail.com>
2023-01-07 18:04:55 -0700

    k8s/tls: Add missing certificate to import script
    The admin subdomains use a separate TLS cert because wildcards don't recurse.

--- k8s/tls/install-secret.sh
+++ k8s/tls/install-secret.sh
@@ -1,1 +1,2 @@
 kubectl create secret tls le-ctf-2023-keypair --cert=../secrets/production/ssl-certificate.pem --key=../secrets/production/ssl-certificate.key
+kubectl create secret tls -n monitoring le-admin-ctf-2023-keypair --cert=../secrets/.lego/certificates/admin.ctf-2023.clubeh.ca.crt --key=../secrets/.lego/certificates/admin.ctf-2023.clubeh.ca.key
