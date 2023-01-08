cc3b1a772cfabd7a198b94a1a481c0ea200b9b94
sudoBash418 <sudoBash418@gmail.com>
2023-01-07 18:06:57 -0700

    k8s/images/ctfd: Add our custom theme to the image

--- k8s/images/ctfd/build.sh
+++ k8s/images/ctfd/build.sh
@@ -10,8 +10,16 @@ if [[ ! -d "ctfd" ]]; then
 	git clone -b deploy 'https://git.sb418.net/sudoBash418/ctfd' "ctfd"
 fi
 
+# clone custom CTFd theme from git if missing
+if [[ ! -d "ctfd/CTFd/themes/ctfd-theme" ]]; then
+	git clone -b hackers-odyssey 'git@github.com:club-eh/ctfd-theme.git' "ctfd-theme"
+fi
+
 # update CTFd repo
 git -C ctfd/ pull
+
+# update CTFd theme repo
+git -C ctfd/CTFd/themes/ctfd-theme pull
 
 # build container image
 podman build -t ctf2023_ctfd -f Containerfile .
